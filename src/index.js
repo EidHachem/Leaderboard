6ef0728882731a9f79c3dd5eb4d7a0bcf93799c4
EidHachem <eidhachem1@gmail.com>
2022-05-11 18:41:40 +0300

    Add set data function and change game url

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './styles/style.scss';
-import { url, refresh } from './modules/variables.js';
+import {
+  url, refresh, form, user, score,
+} from './modules/variables.js';
 
 const userScoreList = document.querySelector('.add-to-board');
 
 userScoreList.innerHTML = '';
 
 const getData = async () => {
-  const response = await fetch(`${url}/games/Zl4d7IVkemOTTVg2fUdz/scores/`);
+  const response = await fetch(`${url}/games/Zl4d7IVkemOTTVg2fEid/scores/`);
   const data = await response.json();
   userScoreList.innerHTML = data.result
     .map((score) => `<li>${score.user}: ${score.score}</li>`)
@@ -14,3 +16,25 @@ const getData = async () => {
 };
 
 refresh.addEventListener('click', getData);
+
+const setData = async (post) => {
+  await fetch(`${url}/games/Zl4d7IVkemOTTVg2fEid/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(post),
+  });
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const post = {
+    user: `${user.value}`,
+    score: `${score.value}`,
+  };
+  setData(post).then(() => {
+    e.target.reset();
+    getData();
+  });
+});
