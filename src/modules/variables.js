5ad19f5cb6b82e28716af2b8a06b5b540f998c7b
EidHachem <eidhachem1@gmail.com>
2022-10-17 19:33:57 +0300

    Updates

--- src/modules/variables.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
-const refresh = document.querySelector('.refresh');
-const form = document.querySelector('.scores-form');
-const user = document.querySelector('#name');
-const score = document.querySelector('#score');
-
-export {
-  url, refresh, form, user, score,
-};
